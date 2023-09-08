0988a956498fa38df7cf621fb55b9b5d7c826778
Dave Drainer <ddrainer@arizona.edu>
2023-09-08 11:28:18 -0700

    update homework working files

---
+++ Homework_Working/week3.py
@@ -0,0 +1,11 @@
+#%%
+mylist=['a', 'b', 'c', 'd']
+print(mylist[1] + mylist[0] + mylist[3])
+
+# %%
+print(mylist[1],mylist[0],mylist[3])
+
+# %%
+test=mylist[1]+mylist[0]+mylist[3]
+print(test)
+# %%
