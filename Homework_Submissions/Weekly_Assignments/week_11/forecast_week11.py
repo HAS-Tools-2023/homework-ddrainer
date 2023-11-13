5ba8bc0639947d287327bf62128b24fcad897141
ddrainer <ddrainer@arizona.edu>
2023-11-12 18:02:26 -0700

    Updated week11 forecast script submission

---
+++ Homework_Submissions/Weekly_Assignments/week_11/forecast_week11.py
@@ -0,0 +1,308 @@
+# %%
+# Using code from HW8, cleaned up using linter.
+
+# Import the modules we will use
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# %%
+# Set the file name, path, read data, and set index
+filename = '../../../data/streamflow_week11.txt'
+filepath = os.path.join('data', filename)
+print(os.getcwd())
+print(filepath)
+
+filepath = '../../../data/streamflow_week11.txt'
+
+# Read the data into a pandas dataframe
+data = pd.read_table(filepath, sep='\t', skiprows=31,
+                     names=['agency_cd', 'site_no',
+                            'datetime', 'flow', 'code'],
+                     parse_dates=['datetime'])
+
+# Set datetime as the index
+data = data.set_index('datetime')
+
+# %%
+# Define Functions
+# Mean Flow Function
+# Inputs required:
+#       dataframe, period (Weekly = 'W', Monthly = 'M', Yearly = 'Y'),
+#       data_col, start_date, and end_date for calc
+
+
+def calc_mean(dataframe, period, data_col, start_date, end_date):
+    df = dataframe[start_date:end_date]  # Filter data based on dates input
+    mean_flow = df.resample(period).mean(data_col)  # Calculate from input
+    return mean_flow
+
+# Max Flow Function
+# Inputs required:
+#       dataframe, period (Weekly = 'W', Monthly = 'M', Yearly = 'Y'),
+#       data_col, start_date, and end_date for calc
+
+
+def calc_max(dataframe, period, data_col, start_date, end_date):
+    df = dataframe[start_date:end_date]  # Filter data based on dates input
+    max_flow = df.resample(period).max(data_col)  # Calculate from input
+    return max_flow
+
+# Min Flow Function
+# Inputs required:
+#       dataframe, period (Weekly = 'W', Monthly = 'M', Yearly = 'Y'),
+#       data_col, start_date, and end_date for calc
+
+
+def calc_min(dataframe, period, data_col, start_date, end_date):
+    df = dataframe[start_date:end_date]  # Filter data based on dates input
+    min_flow = df.resample(period).max(data_col)  # Calculate from input
+    return min_flow
+
+# %%
+# Test Calculate
+
+# Monthly Tests
+# Set variables
+
+
+time_period = 'M'
+start_date = '2023-01-01'
+end_date = '2023-11-11'
+
+calc_mean(data, time_period, 'flow', start_date, end_date)
+calc_max(data, time_period, 'flow', start_date, end_date)
+calc_min(data, time_period, 'flow', start_date, end_date)
+
+# Weekly Tests
+# Set variables
+
+time_period = 'W'
+
+calc_mean(data, time_period, 'flow', start_date, end_date)
+calc_max(data, time_period, 'flow', start_date, end_date)
+calc_min(data, time_period, 'flow', start_date, end_date)
+
+# %%
+# Plot Monthly Average flow for selected time
+
+mean_period = 'M'  # Set period to calculate mean for
+
+mean_plot_data_2023 = calc_mean(data, mean_period, 'flow', '2023-01-01',
+                                '2023-11-11')
+mean_plot_data_2022 = calc_mean(data, mean_period, 'flow', '2022-01-01',
+                                '2022-12-31')
+mean_plot_data_2021 = calc_mean(data, mean_period, 'flow', '2021-01-01',
+                                '2021-12-31')
+mean_plot_data_2020 = calc_mean(data, mean_period, 'flow', '2020-01-01',
+                                '2020-12-31')
+mean_plot_data_2019 = calc_mean(data, mean_period, 'flow', '2019-01-01',
+                                '2019-12-31')
+mean_plot_data_2018 = calc_mean(data, mean_period, 'flow', '2018-01-01',
+                                '2018-12-31')
+
+# Plot all monthly averages from 2018-2023
+
+fig, ax = plt.subplots()
+ax.plot(mean_plot_data_2023['flow'], color='black', label='2023')
+ax.plot(mean_plot_data_2022['flow'], color='green', label='2022')
+ax.plot(mean_plot_data_2021['flow'], color='yellow', label='2021')
+ax.plot(mean_plot_data_2020['flow'], color='red', label='2020')
+ax.plot(mean_plot_data_2019['flow'], color='purple', label='2019')
+ax.plot(mean_plot_data_2018['flow'], color='orange', label='2018')
+ax.set(title="Monthly Avg Flow 2018-2023", xlabel="Date",
+       ylabel="Flow [cfs]", yscale='log')  # Set plot attributes
+ax.set_facecolor('gray')  # Set the background color
+plt.xticks(rotation=45)  # Rotate the x-axis labels 45°
+plt.grid()  # Turn on the grid
+plt.legend()  # Plot the legend
+
+# %%
+# Plot Weekly Average flow for selected time
+
+mean_period = 'W'  # Set period to calculate mean for
+
+mean_plot_data_2023 = calc_mean(data, mean_period, 'flow', '2023-01-01',
+                                '2023-10-28')
+mean_plot_data_2022 = calc_mean(data, mean_period, 'flow', '2022-01-01',
+                                '2022-12-31')
+mean_plot_data_2021 = calc_mean(data, mean_period, 'flow', '2021-01-01',
+                                '2021-12-31')
+mean_plot_data_2020 = calc_mean(data, mean_period, 'flow', '2020-01-01',
+                                '2020-12-31')
+mean_plot_data_2019 = calc_mean(data, mean_period, 'flow', '2019-01-01',
+                                '2019-12-31')
+mean_plot_data_2018 = calc_mean(data, mean_period, 'flow', '2018-01-01',
+                                '2018-12-31')
+
+# Plot all weekly averages from 2018-2023
+
+fig, ax = plt.subplots()
+ax.plot(mean_plot_data_2023['flow'], color='black', label='2023')
+ax.plot(mean_plot_data_2022['flow'], color='green', label='2022')
+ax.plot(mean_plot_data_2021['flow'], color='yellow', label='2021')
+ax.plot(mean_plot_data_2020['flow'], color='red', label='2020')
+ax.plot(mean_plot_data_2019['flow'], color='purple', label='2019')
+ax.plot(mean_plot_data_2018['flow'], color='orange', label='2018')
+ax.set(title="Weekly Avg Flow 2018-2023", xlabel="Date",
+       ylabel="Flow [cfs]", yscale='log')  # Set plot attributes
+ax.set_facecolor('gray')  # Set the background color
+plt.xticks(rotation=45)  # Rotate the x-axis labels 45°
+plt.grid()  # Turn on the grid
+plt.legend()  # Plot the legend
+
+# %%
+# Get Weekly Mean Flow for Oct and Nov
+
+# Set variables
+year = 2018
+month = 10
+
+# Resample original data by week and calc max, min, mean
+wmax_flow = data['flow'].resample('W').max()
+wmin_flow = data['flow'].resample('W').min()
+wmean_flow = data['flow'].resample('W').mean()
+
+# Filter for the month of Oct
+oct_wmax = wmax_flow[wmax_flow.index.month == month]
+oct_wmin = wmin_flow[wmin_flow.index.month == month]
+oct_wmean = wmean_flow[wmean_flow.index.month == month]
+
+# Filter for recent years
+oct_recent_max = oct_wmax[oct_wmax.index.year >= year]
+oct_recent_min = oct_wmin[oct_wmin.index.year >= year]
+oct_recent_avg = oct_wmean[oct_wmean.index.year >= year]
+
+# Calculate min, max, and mean for recent years in Oct
+oct_recent_max = pd.DataFrame(oct_recent_max)
+oct_recent_min = pd.DataFrame(oct_recent_min)
+oct_recent_avg = pd.DataFrame(oct_recent_avg)
+
+# Save cacluations to dataframes for easier use
+oct_wmean = pd.DataFrame(oct_wmean)
+oct_wmax = pd.DataFrame(oct_wmax)
+oct_wmin = pd.DataFrame(oct_wmin)
+
+# Set variables
+year = 2018
+month = 11
+
+# Filter for the month of Nov
+nov_wmax = wmax_flow[wmax_flow.index.month == month]
+nov_wmin = wmin_flow[wmin_flow.index.month == month]
+nov_wmean = wmean_flow[wmean_flow.index.month == month]
+
+# Filter for recent years in Nov
+nov_recent_max = nov_wmax[nov_wmax.index.year >= year]
+nov_recent_min = nov_wmin[nov_wmin.index.year >= year]
+nov_recent_avg = nov_wmean[nov_wmean.index.year >= year]
+
+# Save cacluations to dataframes for easier use
+nov_recent_max = pd.DataFrame(nov_recent_max)
+nov_recent_min = pd.DataFrame(nov_recent_min)
+nov_recent_avg = pd.DataFrame(nov_recent_avg)
+
+nov_wmean = pd.DataFrame(nov_wmean)
+nov_wmax = pd.DataFrame(nov_wmax)
+nov_wmin = pd.DataFrame(nov_wmin)
+
+# %%
+# Plot Oct vs Nov Data
+
+# Set values for x axes
+x_values_subplot1 = oct_recent_max.index.day
+x_values_subplot2 = nov_recent_max.index.day
+
+# Data for the first subplot with 1 row, 2 columns
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4), sharey=True)
+ax1.plot(oct_recent_max['flow'], color='r', label='Weekly Max')
+ax1.plot(oct_recent_min['flow'], color='b', label='Weekly Min')
+ax1.plot(oct_recent_avg['flow'], color='k', label='Weekly Avg')
+ax1.set(title="Oct Weekly Flow Since 2018", xlabel="Date",
+        ylabel="Flow [cfs]", ylim=(0, 300))
+ax1.grid(True)
+ax1.set_xticklabels(x_values_subplot1, rotation=45)
+ax1.legend()
+
+# Data for the second subplot
+ax2.plot(nov_recent_max['flow'], color='orange', label='Weekly Max')
+ax2.plot(nov_recent_min['flow'], color='purple', label='Weekly Min')
+ax2.plot(nov_recent_avg['flow'], color='green', label='Weekly Avg')
+ax2.set(title="Nov Weekly Flow Since 2018", xlabel="Date",
+        ylabel="Flow [cfs]")
+ax2.grid(True)
+ax2.set_xticklabels(x_values_subplot2, rotation=45)
+ax2.legend()
+
+# %%
+# Get October Data Based on Timeseries and Make Forecasts
+
+####################
+# Get Weekly Data of interest
+monthly_data = data['2023-10-01':'2023-11-11']
+last_week = data['2023-11-05':'2023-11-11']
+last_4weeks = data['2023-10-15':'2023-11-11']
+oct_means = oct_wmean['flow'].mean()
+nov_means = nov_wmean['flow'].mean()
+
+# Calculate Avgs for last week and last 4 weeks
+last_week_avg = last_week['flow'].mean()
+last_4weeks_avg = last_4weeks['flow'].mean()
+
+# Make a Forecast based on intuition
+week1_fcst = int(nov_means - (monthly_data.loc['2023-11-11', 'flow']) +
+                 last_week_avg*0.75)
+week2_fcst = int(nov_means - (monthly_data.loc['2023-11-11', 'flow']) +
+                 last_4weeks_avg)
+
+# Plot Data with Avg comapred to observed flow
+fix, ax = plt.subplots()
+ax.plot(monthly_data['flow'], color='orange',
+        label='Observed Flow')
+ax.plot(wmean_flow['2023-10-01':'2023-11-12'], color='gray',
+        label='Weekly Mean')
+ax.plot(hline=last_week_avg)
+ax.plot(hline=last_4weeks_avg)
+ax.plot(hline=week1_fcst)
+ax.plot(hline=week2_fcst)
+
+# Set limits for horizontal line for avgs
+x_min, x_max = ax.get_xlim()
+horizontal_value = last_week_avg
+horizontal_value2 = last_4weeks_avg
+horizontal_value3 = week1_fcst
+horizontal_value4 = week2_fcst
+
+# Plot horizontal Line Avg
+ax.hlines(horizontal_value, x_min, x_max, color='red', linestyle='--',
+          linewidth=2)
+ax.text(x_max, horizontal_value, 'Last Week Avg', va='center', ha='left',
+        color='red')
+
+ax.hlines(horizontal_value2, x_min, x_max, color='purple', linestyle='--',
+          linewidth=2)
+ax.text(x_max, horizontal_value2, '4-Week Avg', va='center', ha='left',
+        color='purple')
+
+ax.hlines(horizontal_value3, x_min, x_max, color='green', linestyle='dotted',
+          linewidth=2)
+ax.text(x_max, horizontal_value3, '1-Week Fcst', va='center', ha='left',
+        color='green')
+
+ax.hlines(horizontal_value4, x_min, x_max, color='blue', linestyle='dotted',
+          linewidth=2)
+ax.text(x_max, horizontal_value4, '2-Week Fcst', va='center', ha='left',
+        color='blue')
+
+# Set Graph Attributes
+ax.set(title="Observed Flow and Forecast", xlabel="Date", ylabel="Flow[cfs]")
+plt.xticks(rotation=45)
+plt.grid()
+plt.legend(loc='lower right')
+
+
+# Print My Forecast and a Message
+print('My week 1 forecast is ', week1_fcst, 'cfs.')
+print('My week 2 forecast is ', week2_fcst, 'cfs.')
+
+# %%
--- Homework_Working/Week_11/Week11_mapping_exercises.py
+++ Homework_Working/Week_11/Week11_mapping_exercises.py
@@ -127,3 +127,192 @@ for axis in [ax.xaxis, ax.yaxis]:
     formatter = ScalarFormatter()
     formatter.set_scientific(False)
     axis.set_major_formatter(formatter)
+
+
+## Earth Analytics Site Code ##
+# %%
+# Import packages
+import os
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import earthpy as et
+
+# Get data and set working directory
+data = et.data.get_data('spatial-vector-lidar')
+os.chdir(os.path.join(et.io.HOME, 'earth-analytics'))
+# %%
+
+# Define path to file
+plot_centroid_path = os.path.join("data", "spatial-vector-lidar", 
+                                  "california", "neon-sjer-site", 
+                                  "vector_data", "SJER_plot_centroids.shp")
+
+# Import shapefile using geopandas
+sjer_plot_locations = gpd.read_file(plot_centroid_path)
+
+# View top 6 rows of attribute table
+sjer_plot_locations.head(6)
+
+# View the geometry type of each row
+sjer_plot_locations.geom_type
+
+# View object type
+type(sjer_plot_locations)
+
+# View CRS of object
+sjer_plot_locations.crs
+
+# %% Lesson 2
+import os 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+import seaborn as sns
+import geopandas as gpd
+from shapely.geometry import Point
+import earthpy as et 
+
+# Adjust plot font sizes
+sns.set(font_scale=1.5)
+sns.set_style("white")
+
+# Set working dir & get data
+data = et.data.get_data('spatial-vector-lidar')
+os.chdir(os.path.join(et.io.HOME, 'earth-analytics'))
+# %%
+# Import world boundary shapefile
+worldBound_path = os.path.join("data", "spatial-vector-lidar", "global", 
+                               "ne_110m_land", "ne_110m_land.shp")
+worldBound = gpd.read_file(worldBound_path)
+
+# %%
+# Plot worldBound data using geopandas
+fig, ax = plt.subplots(figsize=(10, 5))
+worldBound.plot(color='darkgrey', 
+                ax=ax)
+# Set the x and y axis labels
+ax.set(xlabel="Longitude (Degrees)",
+       ylabel="Latitude (Degrees)",
+       title="Global Map - Geographic Coordinate System - WGS84 Datum\n Units: Degrees - Latitude / Longitude")
+
+# Add the x y graticules
+ax.set_axisbelow(True)
+ax.yaxis.grid(color='gray', 
+              linestyle='dashed')
+ax.xaxis.grid(color='gray', 
+              linestyle='dashed')
+# %%
+# Create numpy array of x,y point locations
+add_points = np.array([[-105.2519,   40.0274], 
+                       [  10.75  ,   59.95  ], 
+                       [   2.9833,   39.6167]])
+
+# Turn points into list of x,y shapely points 
+city_locations = [Point(xy) for xy in add_points]
+city_locations  # Why do I need to type this twice?
+# %%
+# Plot point locations
+fig, ax = plt.subplots(figsize=(12, 8))
+
+worldBound.plot(figsize=(10, 5), color='k',
+               ax=ax)
+# Add city locations
+city_locations.plot(ax=ax, 
+                    color='springgreen', 
+                    marker='*',
+                    markersize=45)
+
+# Create numpy array of x,y point locations
+add_points = np.array([[-105.2519,   40.0274], 
+                       [  10.75  ,   59.95  ], 
+                       [   2.9833,   39.6167]])
+
+# Turn points into list of x,y shapely points 
+city_locations = [Point(xy) for xy in add_points]
+city_locations
+
+# Setup x y axes with labels and add graticules
+ax.set(xlabel="Longitude (Degrees)", ylabel="Latitude (Degrees)",
+       title="Global Map - Geographic Coordinate System - WGS84 Datum\n Units: Degrees - Latitude / Longitude")
+ax.set_axisbelow(True)
+ax.yaxis.grid(color='gray', linestyle='dashed')
+ax.xaxis.grid(color='gray', linestyle='dashed')
+# %%
+# Create geodataframe using the points
+city_locations = gpd.GeoDataFrame(city_locations, 
+                                  columns=['geometry'],
+                                  crs=worldBound.crs)
+city_locations.head(3)
+
+# Import graticule & world bounding box shapefile data
+graticule_path = os.path.join("data", "spatial-vector-lidar", "global", 
+                              "ne_110m_graticules_all", "ne_110m_graticules_15.shp")
+graticule = gpd.read_file(graticule_path)
+
+bbox_path = os.path.join("data", "spatial-vector-lidar", "global", 
+                         "ne_110m_graticules_all", "ne_110m_wgs84_bounding_box.shp")
+bbox = gpd.read_file(bbox_path)
+
+# Create map axis object
+fig, ax = plt.subplots(1, 1, figsize=(15, 8))
+
+# Add bounding box and graticule layers
+bbox.plot(ax=ax, alpha=.1, color='grey')
+graticule.plot(ax=ax, color='lightgrey')
+worldBound.plot(ax=ax, color='black')
+
+# Add points to plot 
+city_locations.plot(ax=ax, 
+                    markersize=60, 
+                    color='springgreen',
+                    marker='*')
+# Add title and axes labels
+ax.set(title="World Map - Geographic Coordinate Reference System (long/lat degrees)",
+       xlabel="X Coordinates (meters)",
+       ylabel="Y Coordinates (meters)");
+# %%
+# Reproject the data
+worldBound_robin = worldBound.to_crs('+proj=robin')
+graticule_robin = graticule.to_crs('+proj=robin')
+
+# Plot the data
+fig, ax = plt.subplots(figsize=(12, 8))
+
+worldBound_robin.plot(ax=ax,
+                      color='k')
+
+graticule_robin.plot(ax=ax, color='lightgrey')
+
+ax.set(title="World Map: Robinson Coordinate Reference System",
+       xlabel="X Coordinates (meters)",
+       ylabel="Y Coordinates (meters)")
+
+for axis in [ax.xaxis, ax.yaxis]:
+    formatter = ScalarFormatter()
+    formatter.set_scientific(False)
+    axis.set_major_formatter(formatter)
+# %%
+# Plot the data
+fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+
+worldBound_robin.plot(ax=ax,
+                      color='k')
+graticule_robin.plot(ax=ax, 
+                     color='lightgrey')
+city_locations.plot(ax=ax, 
+                    marker='*', 
+                    color='springgreen', 
+                    markersize=100)
+
+ax.set(title="World Map: Robinson Coordinate Reference System", 
+       xlabel="X Coordinates (meters)",
+       ylabel="Y Coordinates (meters)")
+
+for axis in [ax.xaxis, ax.yaxis]:
+    formatter = ScalarFormatter()
+    formatter.set_scientific(False)
+    axis.set_major_formatter(formatter)
+    
+plt.axis('equal');    
+# %%
